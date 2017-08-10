9586e9a9b741ed67045cbfe3ace03c9f50475558
DAMMIKA <DAMMIKA>
2017-08-10 07:22:26 +0530

    routing basics

--- WebApp/AngularWebApp/client/src/app/app.module.ts
+++ WebApp/AngularWebApp/client/src/app/app.module.ts
@@ -1,11 +1,20 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { Module as AModule} from './moduleA/module';
 import { Module as BModule } from './moduleB/module';
 
-let modb = require('./moduleB/module');
+import { ContainerComponent as AComponent } from './moduleA/components/container.component'
+import { ContainerComponent as BComponent } from './moduleB/components/container.component'
+
+
+const appRoutes: Routes = [
+    { path: 'a', component: AComponent },
+    { path: 'b', component: BComponent }, // <-- delete this line
+    { path: '', redirectTo: '/a', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +22,11 @@ let modb = require('./moduleB/module');
   ],
   imports: [
       BrowserModule,
+      RouterModule.forRoot(
+          appRoutes,
+          { enableTracing: true }
+      ),
+
       AModule,
       BModule
   ],
