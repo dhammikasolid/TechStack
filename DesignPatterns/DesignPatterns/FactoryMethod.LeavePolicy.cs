869256d9a2042f066b142e653f30bae9240deb0b
dammika <apps.dammika@gmail.com>
2017-11-16 22:59:13 +0530

    Factory method

---
+++ DesignPatterns/DesignPatterns/FactoryMethod.LeavePolicy.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.FactoryMethod
+{
+    // Dependencies
+
+        // DTO
+    public interface LeaveRequest
+    {
+
+    }
+        // Injected Dependency
+    public interface LeaveService
+    {
+
+    }
+
+    // Product
+    public abstract class LeaveRule
+    {
+        public abstract bool CheckEligibility(LeaveRequest  leaveRequest, LeaveService leaveService);
+
+        public abstract string GetFailingMessage();
+    }
+
+    // Concrete Product
+    public class NConsecutiveLeaves : LeaveRule
+    {
+        public override bool CheckEligibility(LeaveRequest leaveRequest, LeaveService leaveService)
+        {
+            return true;
+        }
+
+        public override string GetFailingMessage()
+        {
+            return "NConsecutiveLeaves Failed";
+        }
+    }
+
+    // Concrete Product
+    public class NDaysToLastLeave : LeaveRule
+    {
+        public override bool CheckEligibility(LeaveRequest leaveRequest, LeaveService leaveService)
+        {
+            return true;
+        }
+
+        public override string GetFailingMessage()
+        {
+            return "NDaysToLastLeave Failed";
+        }
+    }
+
+    // Creator
+    public abstract class LeavePolicy
+    {
+        protected IList<LeaveRule> rules = new List<LeaveRule>();
+
+        public bool CheckEligibility(LeaveRequest leaveRequest, LeaveService leaveService)
+        {
+            foreach (var rule in rules)
+            {
+                if (!rule.CheckEligibility(leaveRequest, leaveService))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public abstract void AddRules();
+    }
+
+    // Concrete Creator
+    public class LeaveAssignmentPolicy: LeavePolicy
+    {
+        public override void AddRules()
+        {
+            rules.Add(new NConsecutiveLeaves());
+        }
+    }
+
+    public class LeaveRequestPolicy : LeavePolicy
+    {
+        public override void AddRules()
+        {
+            rules.Add(new NConsecutiveLeaves());
+            rules.Add(new NDaysToLastLeave());
+        }
+    }
+
+    // Factory Method 2nd form
+    //Concentrate Creator with Factory Method
+    public class LeavePolicyManager
+    {
+        public LeavePolicy GetLeavePolicy(string type, LeaveRequest leaveRequest, LeaveService leaveService)
+        {
+            if (type == "Assignment")
+                return new LeaveAssignmentPolicy();
+            else if (type == "Request")
+                return new LeaveRequestPolicy();
+
+            return null;
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/FactoryMethod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.FactoryMethod
+{
+    public abstract class Product
+    {
+        public abstract void Do();
+    }
+
+    public class Product1 : Product
+    {
+        public override void Do()
+        {
+            Console.WriteLine("Product1 Do()");
+        }
+    }
+
+    public class Product2 : Product
+    {
+        public override void Do()
+        {
+            Console.WriteLine("Product2 Do()");
+        }
+    }
+
+    public abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    public class Product1Creator : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new Product1();
+        }
+    }
+
+    public class Product2Creator : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new Product2();
+        }
+    }
+
+    public class Client
+    {
+        public Client()
+        {
+            bool condition = true;
+
+            Product _product;
+
+            if (condition)
+                _product = new Product1Creator().FactoryMethod();
+            else
+                _product = new Product2Creator().FactoryMethod();
+
+            _product.Do();
+        }
+    }
+}
