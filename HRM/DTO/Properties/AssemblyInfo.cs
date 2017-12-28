dc9bb3844795012840f6f57d05ef42e0b2a7efc0
Dammika Nayanajith <dhammikasolid@gmail.com>
2017-12-28 09:57:55 -0330

    share

--- HRM/DTO/Properties/AssemblyInfo.cs
+++ HRM/DTO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Services")]
+[assembly: AssemblyTitle("DTO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Services")]
+[assembly: AssemblyProduct("DTO")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e3ced058-e6c9-4468-9c79-c668da905c28")]
+[assembly: Guid("20326300-e495-4df2-8c93-63f2e18849d5")]
 
 // Version information for an assembly consists of the following four values:
 //
