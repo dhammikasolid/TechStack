67f693c6adad0d212f52cc35812d79ee15e80e9a
Dammika Nayanajith <dhammikasolid@gmail.com>
2018-05-17 16:31:54 +0530

    SQl rate schedule implementation

--- SQL/RateSchedules.sql
+++ SQL/RateSchedules.sql
@@ -1,54 +1,97 @@
-CREATE TABLE [dbo].[RateSchedule](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[From] [datetime] NULL,
-	[To] [datetime] NULL,
-	[Rate] [int] NULL,
- CONSTRAINT [PK_RateSchedule] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
 USE [RefImpls]
-GO
 
-CREATE TABLE [dbo].[ReservationSchedule](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[From] [datetime] NULL,
-	[To] [datetime] NULL,
- CONSTRAINT [PK_ReservationSchedule] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+insert into ReservationSchedule ([From], [To]) values
+	('2010-01-01 08:00:00', '2010-01-01 12:00:00'),
+	('2010-01-02 10:00:00', '2010-01-02 12:00:00'),
+	('2010-01-03 12:00:00', '2010-01-03 18:00:00'),
+	('2010-01-04 08:00:00', '2010-01-04 16:00:00');
 
-insert into RateSchedule  ([From], [To], Rate)
-select '2010-01-01 10:00:00', '2010-01-01 12:00:00', 1000 union all
-select '2010-01-02 14:00:00', '2010-01-02 16:00:00', 2000
-
-insert into ReservationSchedule ([From], [To])
-select '2010-01-01 08:00:00', '2010-01-01 18:00:00' union all
-select '2010-01-02 08:00:00', '2010-01-02 18:00:00'
-
+insert into RateSchedule  ([From], [To], Rate) values 
+	('2010-01-01 10:00:00', '2010-01-01 14:00:00', 1),
+	('2010-01-02 08:00:00', '2010-01-02 14:00:00', 2),
+	('2010-01-03 10:00:00', '2010-01-03 14:00:00', 3),
+	('2010-01-04 10:00:00', '2010-01-04 14:00:00', 4);
 
 if object_id('tempdb..#BreakDown') is not null
 	drop table #BreakDown;
 
 create table #BreakDown
 (
-	[From] datetime,
-	[To] datetime,
-	[Rate] int,
+	[From]	datetime,
+	[To]	datetime,
+	[Rate]	int,
+	[STG]	varchar(10),
+	[SF]	datetime,
+	[ST]	datetime,
+	[RF]	datetime,
+	[RT]	datetime,
 )
 
+
 insert into #BreakDown
-select rt.[From], rt.[To], rt.[Rate] from ReservationSchedule rs
-join RateSchedule rt on		rt.[From] between rs.[From] and rs.[To]
-						and  rt.[To] between rs.[From] and rs.[To]
+--[1] S T S T (*) --
+select t.[From], s.[To], t.[Rate], 'S T S T', s.[From], s.[To], t.[From], t.[To] 
+from ReservationSchedule s
+join RateSchedule t on			t.[From] between s.[From] and s.[To]
+						and		s.[To] between t.[From] and t.[To]
+union all
+--[2] T S S T (SS < TT)--
+select s.[From], s.[To], t.[Rate], 'T S S T', s.[From], s.[To], t.[From], t.[To] 
+from ReservationSchedule s
+join RateSchedule t on			s.[From] between t.[From] and t.[To]
+						and		s.[To] between t.[From] and t.[To]
+union all
+--[3] T S T S (*)--
+select s.[From], t.[To], t.[Rate], 'T S T S', s.[From], s.[To], t.[From], t.[To] 
+from ReservationSchedule s
+join RateSchedule t on			s.[From] between t.[From] and t.[To]
+						and		t.[To] between s.[From] and s.[To]
+--[4] S T T S (SS > TT)--
+union all
+select t.[From], t.[To], t.[Rate], 'S T T S', s.[From], s.[To], t.[From], t.[To] 
+from ReservationSchedule s
+join RateSchedule t on			t.[From] between s.[From] and s.[To]
+						and		t.[To] between s.[From] and s.[To]
 
 select * from #BreakDown;
+
+select * from ReservationSchedule;
+
+delete s
+--[2] T S S T (SS < TT)--
+from ReservationSchedule s
+join RateSchedule t on			s.[From] between t.[From] and t.[To]
+						and		s.[To] between t.[From] and t.[To]
+
+select * from ReservationSchedule;
+
+update ReservationSchedule
+set [To] = t.[From]
+--[1] S T S T (*) --
+from ReservationSchedule s
+join RateSchedule t on			t.[From] between s.[From] and s.[To]
+						and		s.[To] between t.[From] and t.[To]
+
+update ReservationSchedule
+set [To] = t.[To]
+--[3] T S T S (*)--
+from ReservationSchedule s
+join RateSchedule t on			s.[From] between t.[From] and t.[To]
+						and		t.[To] between s.[From] and s.[To]
+
+select * from ReservationSchedule;
+
+--[4] S T T S (SS > TT)--
+insert into ReservationSchedule
+select s.[From], t.[To] 
+from ReservationSchedule s
+join RateSchedule t on			t.[From] between s.[From] and s.[To]
+						and		t.[To] between s.[From] and s.[To]
+
+select * from ReservationSchedule;
 
 truncate table ReservationSchedule;
 truncate table RateSchedule;
 truncate table #BreakDown;
+
+
