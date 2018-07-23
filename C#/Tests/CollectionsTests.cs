13f4e2cba012f26bdd102fa59d5ab1ded0b5a1b1
Dammika Nayanajith <dhammikasolid@gmail.com>
2018-07-23 08:06:09 -0500

    C# lambda expressions, IEnumarable, IQueryable

--- C#/Tests/CollectionsTests.cs
+++ C#/Tests/CollectionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,39 @@ namespace UnitTests
     public class CollectionsTests
     {
         [Fact]
-        public void List_Test()
+        public void List()
         {
+            var list = new List<int> { 4, 2, 5 };
+            list.Add(7);
+
+            // Index
+            Assert.Equal(7, list[list.Count - 1]);
+
+            // Without Linq
+            Assert.Equal(2, list.Find(i => i == 2));
+        }
+
+        [Fact]
+        public void List_Remove_Test()
+        {
+            // Use IEquatable<T> for complex find, remove, contain tests
+            var list = new List<int> { 4, 2, 5 };
+
+            if (list.Contains(1))
+                list.Remove(1);
+            Assert.Equal(3, list.Count);
+
+            // Use Linq to find and Remove item
+            var item = list.FirstOrDefault(i => i == 2);
+            list.Remove(item);
+
+            Assert.DoesNotContain(2, list);
+        }
+
+        [Fact]
+        public void List_Sort_Test()
+        {
+            // Use IComparable for complex ordering
             Assert.Equal(-1, 10.CompareTo(12));
             Assert.Equal(0, 10.CompareTo(10));
             Assert.Equal(1, 10.CompareTo(8));
@@ -28,6 +60,7 @@ namespace UnitTests
             Assert.Equal(5, list2[0]);
             Assert.Equal(2, list2[2]);
 
+            // Use Linq for basic acs, desc ordering
             var list3 = new List<int> { 4, 2, 5 };
             list3 = list3.OrderByDescending(i => i).ToList();
 
@@ -38,13 +71,28 @@ namespace UnitTests
         [Fact]
         public void SortedList_Test()
         {
-            var sortedList = new SortedList<string, int>();
-            sortedList.Add("c", 10);
-            sortedList.Add("a", 8);
-            sortedList.Add("b", 15);
+            var sortedList = new SortedList<string, int>
+            {
+                { "c", 10 },
+                { "a", 8 },
+                { "b", 15 }
+            };
 
             Assert.Equal("a", sortedList.ToList()[0].Key);
             Assert.Equal("c", sortedList.ToList()[2].Key);
+        }
+
+        [Fact]
+        public void Dictionary_Test()
+        {
+            var dic = new Dictionary<int, string>
+            {
+                { 5, "Item 3" },
+                { 2, "Item 2" },
+                { 7, "Item 5" },
+            };
+
+            Assert.Equal("Item 2", dic[2]);
         }
     }
 }
---
+++ C#/Tests/LambdaExpressionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LambdaExpressionsTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Func<int, bool> del = x => x == 2; // Use for create delegate
+            Assert.True(del(2));
+
+            IEnumerable<int> enumerable = new List<int> { };
+            enumerable.Where(del); // Extention from System.Linq.Enumerable
+
+            IQueryable<int> queryable = enumerable.AsQueryable();
+            Expression<Func<int, bool>> expression = x => x == 2; // Use for create Expression
+            queryable.Where(expression); // Extention from System.Linq.Queryable
+        }
+
+        int M(int x) { return x; }
+
+        [Fact]
+        public void ExpressionLambdas_Test()
+        {
+            Func<int, bool> del1 = x => x == 2; // Single input
+            Func<int, int, bool> del2 = (x, y) => x == y; // Single input
+            Func<bool> del3 = () => 2 == 2; // No input 
+            Func<int> del4 = () => M(2); // Calling methods has no meaning outside CLR (SQL-Server)
+        }
+
+        [Fact]
+        public void StatementLambdas_Test()
+        {
+            Func<int, bool> del = x => {
+                int y = x % 5;
+                return y == 0;
+            };
+
+            //Like anonymous methods, cannot be used to create expression trees
+        }
+    }
+}
